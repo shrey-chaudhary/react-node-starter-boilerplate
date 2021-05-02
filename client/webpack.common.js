b89670715e4a2a043270a0b2a84c2b4726d8a63d
shrey chaudhary <shreychaudhary1995@gmail.com>
2021-05-02 10:49:37 +0530

    move build folder up

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
         index: path.resolve(__dirname, 'src', 'index.js'),
     },
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, '..', 'build'),
     },
     module: {
         rules: [{
